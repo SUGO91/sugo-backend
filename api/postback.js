4a6a2d52480df73bee2113de74c59b499c1458e5
SUGO91 <primemuneeb91@gmail.com>
2026-06-21 14:20:07 +0530

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -1,5 +1,14 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    message: "Sugo backend is working 🚀"
-  });
-}
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      message: "Sugo backend running 🚀"
+    })
+  );
+});
+
+server.listen(10000, () => {
+  console.log("Server running on port 10000");
+});
